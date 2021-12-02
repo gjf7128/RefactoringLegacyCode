bd4fc99289433241c7e326ba9c4193129740c148
gjf7128 <gjf7128@rit.edu>
2021-12-02 06:16:23 -0500

    enabled transition to spare state

--- src/Normal.java
+++ src/Normal.java
@@ -18,9 +18,8 @@ public class Normal implements FrameState {
         }
         for (int i = 0; i != 20; i++) {
             // if a spare happens
-            if (i == 1 && curScore[i + counter] == 10) {
-                lane.getCumulScores()[lane.getBowlIndex()][i+counter] += curScore[i+counter];
-                counter += 2;
+            if (i % 2 == 1 && curScore[i] == 10) {
+                lane.getCumulScores()[lane.getBowlIndex()][(i-1)/2] += curScore[i];
                 lane.setFrameState(lane.getSpareState());
             }
 //            if (curScore[0] + curScore[1] == 10) {
